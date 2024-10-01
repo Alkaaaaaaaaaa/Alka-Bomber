e62b15a05d438b35b842b3a47c8feed7770f6d20
Alka <ejeurbbeueve@gmail.com>
2024-10-01 19:05:55 +0200

    Update Launch-Win.bat

--- Launch-Win.bat
+++ Launch-Win.bat
@@ -1,3 +1,17 @@
 @echo off
-python Alka-Bomber.py
-pause
+
+set PYTHON_EXECUTABLE=python.exe
+
+set SCRIPT_FILE=Alka-Bomber.py
+
+if not exist "%PYTHON_EXECUTABLE%" (
+  echo Error: Python executable not found at %PYTHON_EXECUTABLE%
+  exit /b 1
+)
+
+if not exist "%SCRIPT_FILE%" (
+  echo Error: Script file not found at %SCRIPT_FILE%
+  exit /b 1
+)
+
+"%PYTHON_EXECUTABLE%" "%SCRIPT_FILE%" %*
